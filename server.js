663c7d8e221df6f02ab64f38f58ed5a8c4bc5359
musherk <79853672+musherk@users.noreply.github.com>
2021-03-02 01:18:00 +0100

    add comment

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ let port = 8000;
 
 app.get('/', (request, response) => response.send('Hello world !!!'));
 
+// launch server
 app.listen(port, function () { console.log("Server running on port " + port);
 })
